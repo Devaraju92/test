2f257bc1ebb9b41d81fef934540598afdbd72625
Devaraju <devarajudg91@gmail.com>
2019-01-18 11:13:47 +0530

    sop statement added

--- git.java
+++ git.java
@@ -3,6 +3,14 @@ class git
 public static void main(String args[])
 {
 System.out.println("hello");
+
+System.out.println("hello1");
+
+System.out.println("hello2");
+
+System.out.println("hello checking ");
+
+
 }
 
 }
